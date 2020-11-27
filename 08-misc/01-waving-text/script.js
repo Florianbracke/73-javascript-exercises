15dd329b8c28c7ed3dd1256cf2af5fa0774327d1
Florianbracke <florianbracke@gmail.com>
2020-11-27 10:18:52 +0100

    misc

--- 08-misc/01-waving-text/script.js
+++ 08-misc/01-waving-text/script.js
@@ -1,16 +1,78 @@
-/* becode/javascript
- *
- * /07-misc/01-waving-text/script.js - 7.1: effet vague
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
+setTimeout(myWave, 2000);
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
+ myWave = () = {
 
-(function() {
+   //select target and make it into an array
+    const target = document.getElementById("target");
+    let dotArray = target.innerHTML.split(" ");
 
-    // your code here
+   //array of fonts to be paired with dotArray 
+    let font = ["10px", "20px", "30px", "40px", "50px"];
+    
+   //for loop to arrange fonts by size (it matters)
+    
+    var i;
+    for (i = 0; i < font.length; i++) {
+    font[i] + " ";
+    }
+  
+   //forEach index dotArray select font
+    dotArray.forEach(element => item.style.fontSizOpne = font[i]);
 
-})();
+
+
+
+
+//     let spansText = [];
+// console.log(spansText)
+ 
+//     }
+
+//     // Replace the text with the different spans and put together with join
+//     targetText.innerHTML = spansText.join(" ");
+}
+
+
+// window.myWave1();
+// // function test(){document.getElementById("target").innerHTML = "ooooooooooo";}
+
+// function myWave1(){ 
+    // var p = document.getElementById("target");
+    // var p = document.createTextNode("span");
+    // span.value="test?";
+    // p.setAttributeNode(span);
+ 
+        // var t   = document.createTextNode("oooooooooo");
+        // var t   = document.createAttribute("class");
+        // // p.value = "test?";
+        // // p.setAttributeNode(span);
+
+        // document.body.appendChild(t);
+        // document.getElementById("p2").style.color = "blue";
+    
+// var anchor = document.getElementById("myAnchor");  // Get the <a> element with id="myAnchor"
+// var att = document.createAttribute("href");        // Create a "href" attribute
+// att.value = "https://www.w3schools.com";            // Set the value of the href attribute
+// anchor.setAttributeNode(att);                      // Add the href attribute to <a>
+
+
+// element.style.fontSize = "300";
+// element.style.fontSize = "400";
+// element.style.fontSize = "500";
+// element.style.fontSize = "600";
+// element.style.fontSize = "700";
+// element.style.fontSize = "800";
+
+//     document.getElementById("target").innerHTML = "OOOOooooooooo";}
+// setTimeout(myWave1, 1000);
+
+// function myWave2(){document.getElementById("target").innerHTML = "ooooOOOOooooo";}
+// setTimeout(myWave2, 2000);
+
+// function myWave3(){document.getElementById("target").innerHTML = "oooooooooOOOO";}
+// setTimeout(myWave3, 3000);
+
+
+// for (i = 0; i < 50; i++) {
+//  text +=    wave[i] + " ";
+//  }   
