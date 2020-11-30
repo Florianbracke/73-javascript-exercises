67f5c9d0d06b6393ee8e13e13421b510905c255e
Florianbracke <florianbracke@gmail.com>
2020-11-30 12:21:40 +0100

    start ex2

--- 09-synchronous/02-get-comments/script.js
+++ 09-synchronous/02-get-comments/script.js
@@ -1,14 +1,23 @@
-/* becode/javascript
- *
- * /10-asynchronous/02-get-comments/script.js - 10.2: chargement d'articles et de commentaires
- *
- * coded by leny@BeCode
- * started at 09/05/2019
- */
+// (() => {
+//     document.getElementById("run").addEventListener("click", function(){
+//         window.lib.getPosts(function(error,articles) {
+//             window.lib.getComments(articles,function (){
+//             console.log(articles-id)
+//             } )
+//         } )
+//     } )
+// })();
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById("run").addEventListener("click", function(){
+        window.lib.getPosts(function(error,articles) {
+            console.log(error, articles)
+            })
+            indow.lib.getComments(post-id, (error, comments), function (){
+            console.log("test")
+        })
+    })
 })();
+
+  
