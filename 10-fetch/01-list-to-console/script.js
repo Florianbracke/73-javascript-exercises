81f0e0f6ce0b5913e1bfa0de431b9705ada1fe16
Florianbracke <florianbracke@gmail.com>
2020-11-30 16:40:29 +0100

    fetch countries

--- 10-fetch/01-list-to-console/script.js
+++ 10-fetch/01-list-to-console/script.js
@@ -1,14 +1,12 @@
-/* becode/javascript
- *
- * /09-fetch/01-list-to-console/script.js - 11.1: liste vers console
- *
- * coded by leny@BeCode
- * started at 12/05/2019
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
 (() => {
-    // your code here
+    
+    fetch("http://restcountries.eu/rest/v2/all/")
+        .then(
+            function(response) {
+                console.log(response);
+    
+                    response.json().then(function(country){
+                        console.log(country);
+            })
+})
 })();
